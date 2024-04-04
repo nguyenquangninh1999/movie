1d22bcd2d4901ab675a6355d2fb12dc02f1012e6
Ninh Nguyen <ninh.nguyen@tomosia.com>
2024-04-04 21:48:30 +0700

    main

--- app/Http/Controllers/Client/MovieClientController.php
+++ app/Http/Controllers/Client/MovieClientController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 
 class MovieClientController extends Controller
 {
     public function index()
     {
-        return view("client.index");
+        $movies = Movie::query()->get();
+
+        return view("client.index", compact('movies'));
     }
 }
